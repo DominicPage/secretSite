9f422e201e593af0ee928632d12701798b083e34
Page <DominicPage@utmostworldwide.com>
2020-08-22 13:28:58 +0100

    replaced bcrypt hash with passport.js authentication for seesion and cookie management

--- app.js
+++ app.js
@@ -4,10 +4,10 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const mongoose = require("mongoose");
-//const encrypt = require("mongoose-encryption");
-//const md5 = require("md5");
-const bcrypt = require("bcrypt");
-const saltRounds = 10;
+const session = require("express-session");
+const passport = require("passport")
+const passportLocalMongoose = require("passport-local-mongoose")
+
 
 const Schema = mongoose.Schema;
 
@@ -21,21 +21,33 @@ app.use(
   })
 );
 
+app.use(session({
+  secret: process.env.PASSPORT_SECRET,
+  resave: false,
+  saveUninitialized: false,
+}))
+app.use(passport.initialize());
+app.use(passport.session());
+
 mongoose.connect(process.env.MONGO_DB_CONNECTION, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 });
 
+mongoose.set("useCreateIndex", true);
+
 const userSchema = new Schema({
   email: String,
   password: String,
 });
-// SCHEMAS SHOULD BE CREATED ABOVE THE SHEEP
-// const conatining secret was cut from here and moved to the .env file
-// LINE BELOW USED WITH MONGO ENCRYPTION
-// userSchema.plugin(encrypt, {  secret: process.env.SECRET,  encryptedFields: ["password"]});
-// MODELS SHOULD BE ADDED BELOW THE SHEEP
+
+userSchema.plugin(passportLocalMongoose);
+
 const UserModel = new mongoose.model("User", userSchema);
+
+passport.use(UserModel.createStrategy());
+passport.serializeUser(UserModel.serializeUser())
+passport.deserializeUser(UserModel.deserializeUser())
 
 app.get("/", (req, res) => {
   res.render("home");
@@ -49,47 +61,49 @@ app.get("/register", (req, res) => {
   res.render("register");
 });
 
+app.get("/secrets", (req, res) => {
+  if (req.isAuthenticated()) {
+    res.render("secrets")
+  } else {
+    res.redirect("/login")
+  }
+})
+
+app.get("/logout", (req, res) => {
+  req.logout();
+  res.redirect("/")
+})
+
 app.post("/register", (req, res) => {
-  bcrypt.hash(req.body.password, saltRounds, (err, hash) => {
-    // Store hash in your password DB.
-    const newUser = new UserModel({
-      email: req.body.username,
-      password: hash,
-    });
-    newUser.save((err) => {
-      if (err) {
-        console.log(err);
-      } else {
-        res.render("secrets");
-      }
-    });
-  });
+  UserModel.register({
+    username: req.body.username
+  }, req.body.password, (err, user) => {
+    if (err) {
+      console.log(err);
+      res.redirect("/register")
+    } else {
+      passport.authenticate("local")(req, res, () => {
+        res.redirect("/secrets");
+      })
+    }
+  })
 });
 
 app.post("/login", (req, res) => {
-  // Store hash in your password DB.
-  const username = req.body.username;
-  const password = req.body.password;
-
-  UserModel.findOne(
-    {
-      email: username,
-    },
-    (err, foundUser) => {
-      if (err) {
-        console.log(err);
-      } else {
-        if (foundUser) {
-          //if (foundUser.password === password) {
-          bcrypt.compare(password, foundUser.password, (err, result) => {
-            if (result === true) {
-              res.render("secrets");
-            }
-          });
-        }
-      }
+  const user = new UserModel({
+    username: req.body.username,
+    password: req.body.password
+  })
+  req.login(user, (err) => {
+    if (err) {
+      console.log(err);
+      res.redirect("/login")
+    } else {
+      passport.authenticate("local")(req, res, () => {
+        res.redirect("/secrets")
+      })
     }
-  );
+  })
 });
 
 app.listen(3000, () => {
